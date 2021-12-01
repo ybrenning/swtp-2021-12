6b0af4914f66b7dc076ec32ab670a4f9bcb27982
Maxim Lemel <maxim.lemel@gmail.com>
2021-12-01 18:49:21 +0100

    [Ferris, Yannick, Maxim L] finished method recognition

--- frontend/greenide/out/extension.js
+++ frontend/greenide/out/extension.js
@@ -5,39 +5,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 const WebviewPanel_1 = require("./WebviewPanel");
+var functions = [];
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "greenide" is now active!');
+    vscode.window.showInformationMessage('GreenIDE is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('greenIDE.start', () => {
+    let disposable = vscode.commands.registerCommand('greenIDE.run', () => {
         // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('GreenIDE is now active!');
-        WebviewPanel_1.WebviewPanel.createOrShow(context.extensionUri);
-    });
-    context.subscriptions.push(disposable);
-    // TODO: register function without having to execute it
-    // Provides command to rerun the analysis
-    let disposable1 = vscode.commands.registerCommand('greenIDE.rerun', () => {
-        //TODO: finish command register
         // Starts procedure and updates webview panel
         runAnalysis();
         WebviewPanel_1.WebviewPanel.createOrShow(context.extensionUri);
     });
-    context.subscriptions.push(disposable1);
+    context.subscriptions.push(disposable);
     // Start DocumentSymbolProvider to find methods
     context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider({ language: "java" }, new JavaDocumentSymbolProvider()));
 }
 exports.activate = activate;
 // Performs analysis
-// Procedure order: 1. retreive funtions, 2. provide methods to backend,
-// 3. retreive analysis from backend, 4. display analysis
+// Procedure order:
+//  1. retreive funtions, (done)
+//  2. provide methods to backend,
+//  3. retreive analysis from backend,
+//  4. display results(Webview and syntax highlighting)
 function runAnalysis() {
+    for (var i = 0; i < functions.length; i++) {
+        console.log(functions[i].name, functions[i].location.range.start.character, functions[i].location.range.end.character);
+    }
     // TODO: do procedure order
 }
 // Implementation of documentSymbolProvider to find all parts of code containing 'kanzi.'
@@ -51,15 +50,28 @@ class JavaDocumentSymbolProvider {
                 var line = document.lineAt(i);
                 // Add found kanzi location to object with line
                 if (line.text.includes("kanzi.")) {
-                    symbols.push({
-                        name: line.text.substr(1),
-                        kind: vscode.SymbolKind.Field,
-                        containerName: containerNumber.toString(),
-                        location: new vscode.Location(document.uri, line.range)
-                    });
-                    containerNumber++;
+                    for (var j = 0; j < line.text.length; j++) {
+                        //Search line for kanzi method
+                        if (!line.text.substring(j).includes("kanzi.")) {
+                            for (var k = j; k < line.text.length; k++) {
+                                //Search for end of full kanzi name
+                                if (line.text.substring(j - 1, k).includes("(")) {
+                                    symbols.push({
+                                        name: line.text.substr(j - 1, (k - 1) - (j - 1)),
+                                        kind: vscode.SymbolKind.Method,
+                                        containerName: containerNumber.toString(),
+                                        location: new vscode.Location(document.uri, new vscode.Range(new vscode.Position(i, j), new vscode.Position(i, k)))
+                                    });
+                                    containerNumber++;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
                 }
             }
+            functions = symbols;
             resolve(symbols);
         });
     }
--- frontend/greenide/src/extension.ts
+++ frontend/greenide/src/extension.ts
@@ -4,6 +4,8 @@
 import * as vscode from 'vscode';
 import { WebviewPanel } from './WebviewPanel';
 
+var functions: { name: string; kind: vscode.SymbolKind; containerName: string; location: vscode.Location; }[] = [];
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -11,31 +13,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "greenide" is now active!');
+    vscode.window.showInformationMessage('GreenIDE is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('greenIDE.start', () => {
+	let disposable = vscode.commands.registerCommand('greenIDE.run', () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('GreenIDE is now active!');
+        // Starts procedure and updates webview panel
+        runAnalysis();
 		WebviewPanel.createOrShow(context.extensionUri);
 	});
 
     context.subscriptions.push(disposable);
-
-    // TODO: register function without having to execute it
-    // Provides command to rerun the analysis
-    let disposable1 = vscode.commands.registerCommand('greenIDE.rerun', () => {
-
-        //TODO: finish command register
-            
-        // Starts procedure and updates webview panel
-        runAnalysis();
-        WebviewPanel.createOrShow(context.extensionUri);
-    });
-
-    context.subscriptions.push(disposable1);
 
     // Start DocumentSymbolProvider to find methods
 	context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(
@@ -44,11 +34,15 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // Performs analysis
-// Procedure order: 1. retreive funtions, 2. provide methods to backend,
-// 3. retreive analysis from backend, 4. display analysis
+// Procedure order:
+//  1. retreive funtions, (done)
+//  2. provide methods to backend,
+//  3. retreive analysis from backend,
+//  4. display results(Webview and syntax highlighting)
 function runAnalysis(){
-    
-    
+    for(var i = 0; i < functions.length; i++){
+        console.log(functions[i].name, functions[i].location.range.start.character ,functions[i].location.range.end.character);
+    }
     // TODO: do procedure order
 
 }
@@ -66,15 +60,29 @@ class JavaDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
                 var line = document.lineAt(i);
                 // Add found kanzi location to object with line
                 if (line.text.includes("kanzi.")) {
-                    symbols.push({
-                        name: line.text.substr(1),
-                        kind: vscode.SymbolKind.Field,
-                        containerName: containerNumber.toString(),
-                        location: new vscode.Location(document.uri, line.range)
-                    })
-                    containerNumber++;
+                    for(var j = 0; j < line.text.length; j++){
+                        //Search line for kanzi method
+                        if(!line.text.substring(j).includes("kanzi.")){
+                            for(var k = j; k < line.text.length; k++){
+                                //Search for end of full kanzi name
+                                if(line.text.substring(j-1, k).includes("(")){
+                                    
+                                    symbols.push({
+                                        name: line.text.substr(j-1, (k-1) - (j-1)),
+                                        kind: vscode.SymbolKind.Method,
+                                        containerName: containerNumber.toString(),
+                                        location: new vscode.Location(document.uri, new vscode.Range(new vscode.Position(i, j), new vscode.Position(i, k)))
+                                    })
+                                    containerNumber++;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
                 }
             }
+            functions = symbols;
             resolve(symbols);
         }); 
     }
