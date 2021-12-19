bffff6e1d1db94cf5359e23014993499d9f64a09
Yannick Brenning <yb63tadu@studserv.uni-leipzig.de>
2021-12-19 17:50:56 +0100

    [Yannick] Removed VSCode API dependency

--- frontend/greenide/out/test/suite/extension.test.js
+++ frontend/greenide/out/test/suite/extension.test.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const assert = require("assert");
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-const vscode = require("vscode");
+// const vscode = require("vscode");
 // import * as myExtension from '../../extension';
 suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Start all tests.');
+    // vscode.window.showInformationMessage('Start all tests.');
     test('Sample test', () => {
         assert.strictEqual(-1, [1, 2, 3].indexOf(5));
         assert.strictEqual(-1, [1, 2, 3].indexOf(0));
