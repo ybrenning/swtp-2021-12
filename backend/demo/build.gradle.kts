9b8670316f3891bd311de385a950a4c0110e2bdb
Richard Otto Zimmermann <rz65xuji@studserv.uni-leipzig.de>
2022-01-19 20:26:57 +0100

    add H2 as dependency

--- backend/demo/build.gradle.kts
+++ backend/demo/build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("com.h2database:h2")
+
 }
 
 tasks.withType<KotlinCompile> {
