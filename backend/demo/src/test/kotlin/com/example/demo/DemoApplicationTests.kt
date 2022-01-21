b5d10fb36298c84a275f29e0251afe9d84c2ba74
yb63tadu <yb63tadu@studserv.uni-leipzig.de>
2022-01-21 13:29:39 +0100

    Added simple test run

--- backend/demo/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ backend/demo/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,7 @@ class DemoApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		assertEquals(0, 1 - 1)
 	}
 
 }
