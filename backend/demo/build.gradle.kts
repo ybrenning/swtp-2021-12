8f5e304fc0e0819e271f015420db0b97c15a29eb
Maxim Lemel <ml94weme@studserv.uni-leipzig.de>
2022-01-28 16:37:41 +0100

    [Yannick MaximL] fixed dependencies

--- backend/demo/build.gradle.kts
+++ backend/demo/build.gradle.kts
@@ -16,14 +16,12 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-    runtimeOnly("com.h2database:h2")
-
 }
 
 tasks.withType<KotlinCompile> {
