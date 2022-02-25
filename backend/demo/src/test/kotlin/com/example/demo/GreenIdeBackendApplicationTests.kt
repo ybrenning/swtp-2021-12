05c8d3ba3573dc9ed2011865ee67b29b6d92e520
Yannick Brenning <yb63tadu@studserv.uni-leipzig.de>
2022-02-25 10:35:25 +0100

    Added @ContextConfiguration

--- backend/demo/src/test/kotlin/com/example/demo/GreenIdeBackendApplicationTests.kt
+++ backend/demo/src/test/kotlin/com/example/demo/GreenIdeBackendApplicationTests.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+@ContextConfiguration(classes=[GreenIdeBackendApplication::class])
 @SpringBootTest(classes=[GreenIdeBackendApplication::class])
 class GreenIdeBackendApplicationTests {
 
