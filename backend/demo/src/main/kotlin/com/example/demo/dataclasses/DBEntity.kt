34c49f99115c02db8f64b04fdec9a45d06dfd207
Louis Roth <lr29sive@studserv.uni-leipzig.de>
2022-03-08 15:18:33 +0100

    fixed DBEntity

--- backend/demo/src/main/kotlin/com/example/demo/dataclasses/DBEntity.kt
+++ backend/demo/src/main/kotlin/com/example/demo/dataclasses/DBEntity.kt
@@ -5,12 +5,14 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 data class DBEntity(
-    @Id
-    val id:             String,                     // primary key
     val softwareSystem: String,                     // to what software system the function belongs
     val functionName:   String,                     //
     val configs:        Map<String, Boolean>,       // Map of configs; different software systems have different number
     val energy:         Double,                     //
     val time:           Double,                     //
     )
+{
+    @Id
+    val id:             String,                     // primary key
+}
 
