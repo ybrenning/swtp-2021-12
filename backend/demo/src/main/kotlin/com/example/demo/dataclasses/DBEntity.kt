ba204c3a846183f78bd985fa6bf047416fcbfe19
r <rz65xuji@uni-leipzig.de>
2022-03-09 12:24:05 +0100

    fix DBEntity

--- backend/demo/src/main/kotlin/com/example/demo/dataclasses/DBEntity.kt
+++ backend/demo/src/main/kotlin/com/example/demo/dataclasses/DBEntity.kt
@@ -1,18 +1,16 @@
 package com.example.demo.dataclasses
 
-import org.springframework.data.annotation.Id
+import org.bson.types.ObjectId
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.annotation.Id
 
 @Document
 data class DBEntity(
     val softwareSystem: String,                     // to what software system the function belongs
-    val functionName:   String,                     //
-    val configs:        Map<String, Boolean>,       // Map of configs; different software systems have different number
-    val energy:         Double,                     //
-    val time:           Double,                     //
-    )
-{
-    @Id
-    val id:             String,                     // primary key
-}
+    val functionName: String,                     //
+    val configs: Map<String, Boolean>,       // Map of configs; different software systems have different number
+    val energy: Double,                     //
+    val time: Double, //
+    @Id val id: String = ObjectId.get().toString(),
+)
 
