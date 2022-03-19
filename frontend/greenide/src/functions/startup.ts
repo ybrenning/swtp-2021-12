07c5353f4903ed6adcd5886116c7ab095b1a32b2
Ferris Kleier <fk89dapa@studserv.uni-leipzig.de>
2022-03-19 21:22:23 +0100

    bugfix with system.json

--- frontend/greenide/src/functions/startup.ts
+++ frontend/greenide/src/functions/startup.ts
@@ -66,7 +66,7 @@ async function formatInput(items: string[], mode: string) {
     var objS = {
         system: '' as any
     };
-    objS.system.push('kanzidd');
+    objS.system = 'kanzidd';
     var jsonS = JSON.stringify(objS,null,'\t');
     const writer = fs.writeFile(folder + '/greenide/system.json', jsonS, 'utf8', callback);
     await writer;
