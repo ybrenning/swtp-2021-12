b969f6740d0eb116d27d9fedab24d877ca3ddc64
Ferris Kleier <fk89dapa@studserv.uni-leipzig.de>
2022-03-20 04:29:06 +0100

    works, request commented out

--- frontend/greenide/out/functions/startup.js
+++ frontend/greenide/out/functions/startup.js
@@ -7,25 +7,27 @@ const getSystem_1 = require("./getSystem");
 const folder = vscode.workspace.workspaceFolders?.map(folder => folder.uri.path)[0];
 const fs = require('fs');
 function startup() {
-    // read defined software system
-    //var softwareSystem = JSON.parse(fs.readFileSync(folder + '/greenide/system.json', 'utf8'));
-    //console.log(softwareSystem);
     var softwareSystem = (0, getSystem_1.getSystem)();
-    // TEST suite
-    console.log('SWS IN STARTUP');
-    console.log(softwareSystem);
-    // TODO: change to getrequest from backend when backend works
-    // create items to parse into json
-    var configItems = [];
-    var locatorItems = [];
+    /*var xmlRequest = require('xhr2');
+    const http = new xmlRequest();
+    const urlGet = 'https://swtp-2021-12-production.herokuapp.com/listOfFunctions/' + softwareSystem;
+
+    http.open("GET", urlGet);
+    http.send();
+    http.onreadystatechange = () => {
+        formatInput(http.responseText,'methods');
+    };*/
     // read provided csv
     var result = fs.readFileSync(folder + '/greenide/csv/data.csv', 'utf-8');
     result = result.split('\n');
-    // get values for configItems and locatorItems
-    configItems = getConfigItems(result[0]);
+    // create items to parse into json
+    var locatorItems = [];
     locatorItems = getLocatorItems(result);
-    formatInput(configItems, 'config');
     formatInput(locatorItems, 'methods');
+    // create items to parse into json
+    var configItems = [];
+    configItems = getConfigItems(result[0]);
+    formatInput(configItems, 'config');
 }
 exports.startup = startup;
 // exctract the config items / top bar arguments from csv
@@ -74,93 +76,23 @@ async function formatInput(items, mode) {
         }
         var jsonM = JSON.stringify(objM, null, '\t');
         fs.writeFileSync(folder + '/greenide/locatorItems.json', jsonM, 'utf8');
+        /*var objM = {
+            methods: [] as any
+        };
+
+        if (items.length > 0) {
+
+            items = JSON.parse(items);
+
+            for (let i = 0; i < items.length; i++) {
+                objM.methods.push(items[i]);
+            }
+
+            var jsonM = JSON.stringify(objM,null,'\t');
+            fs.writeFileSync(folder + '/greenide/locatorItems.json', jsonM, 'utf8');
+        }*/
     }
 }
 // useless, just for reading file to work
 function callback(arg0, json, arg2, callback) { }
-// TODO: implementation when backend works, save response
-// function to parse provided csv data into seperate json files to read them later
-/*
-import axios from 'axios';
-import * as vscode from 'vscode';
-
-const folder = vscode.workspace.workspaceFolders?.map(folder => folder.uri.path)[0];
-const fs = require('fs');
-const softwareSystem = 'kanzi';
-
-export function startup() {
-
-    // create items to parse into json
-    var configItems: string[] = [];
-    var document;
-
-    // get data from backend
-    const urlGet='https://swtp-2021-12-production.herokuapp.com/listOfFunctions/' + softwareSystem + '/';
-    axios.get(urlGet)
-    .then(data=>(document=data))
-    .catch(err=>console.log(err));
-
-    // read provided csv
-    var result = fs.readFileSync(folder + '/greenide/csv/data.csv', 'utf-8');
-    result = result.split('\n');
-
-    // get values for configItems
-    configItems = getConfigItems(result[0]);
-
-    // format configItems
-    formatInput(configItems,'config');
-    formatInput(document,'methods');
-}
-
-// exctract the config items / top bar arguments from csv
-function getConfigItems(document: string) {
-
-    var items: string[] = [];
-
-    // slice front and end from line
-    var startIndex = document.indexOf(',');
-    var lastIndex = document.indexOf('run_time(ms;<)');
-    var line = document.slice(startIndex+1,lastIndex-2);
-
-    // seperate each item from its comma, like real csv's
-    items = line.split(',');
-    for (let i = 0; i < items.length; i++) {
-        items[i] = items[i].slice(1,items[i].length-1);
-    }
-
-    return items;
-}
-
-async function formatInput(items: any, mode: string) {
-
-    // declare software system, default for customer is kanzi, can be changed
-    var objS = {
-        system: '' as any
-    };
-    objS.system = 'kanzi';
-    var jsonS = JSON.stringify(objS,null,'\t');
-    const writer = fs.writeFile(folder + '/greenide/system.json', jsonS, 'utf8', callback);
-    await writer;
-
-    if (mode.match('config')) {
-
-        var objC = {
-            items: [] as any
-        };
-        for (let i = 0; i < items.length; i++) {
-            objC.items.push(items[i]);
-        }
-
-        var jsonC = JSON.stringify(objC,null,'\t');
-        fs.writeFile(folder + '/greenide/configItems.json', jsonC, 'utf8', callback);
-    } else {
-
-        // parse response from server into locatorItems.json
-        var jsonM = JSON.stringify(document,null,'\t');
-        fs.writeFile(folder + '/greenide/locatorItems.json', jsonM, 'utf8', callback);
-    }
-}
-
-function callback(arg0: string, json: string, arg2: string, callback: any) { }
-*/ 
 //# sourceMappingURL=startup.js.map
