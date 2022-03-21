bef29c6541e487e9e6f9e2fc1f006aa94c56f400
Maxim Lemel <ml94weme@studserv.uni-leipzig.de>
2022-03-21 10:03:15 +0100

    bugfixing printouts for console

--- backend/demo/src/main/kotlin/com/example/demo/FunctionValueCalculatorService.kt
+++ backend/demo/src/main/kotlin/com/example/demo/FunctionValueCalculatorService.kt
@@ -28,7 +28,11 @@ class FunctionValueCalculatorService(
             for (functionConfigRaw in functionConfigsRaw) {
                 // repeat for every configuration that is requested
                 for (configToFind in configsToFind) {
-                    // is this config {requested} AND {the one in this entry}
+                    //TODO: remove test
+                    println("-------------")
+                    println(configToFind)
+                    println(functionConfigRaw.configs[configToFind])
+                    // is this config {requested} AND {in this entry}
                     if (functionConfigRaw.configs[configToFind] == true) {
                         functionResultEnergy += functionConfigRaw.energy
                         functionResultTime += functionConfigRaw.time
@@ -36,7 +40,7 @@ class FunctionValueCalculatorService(
                     }
                 }
             }
-
+cd C:/GreenIDE/swtp-2021-12
             // add the function to the return list
             configuredFunctions.add(
                 ConfiguredFunction(
