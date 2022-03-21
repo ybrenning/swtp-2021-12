c6271935448322bd07bec1ab91602610c7227783
Maxim Lemel <ml94weme@studserv.uni-leipzig.de>
2022-03-21 21:11:57 +0100

    idk, something with tests

--- backend/demo/src/test/kotlin/com/example/demo/ApplicationControllerTests.kt
+++ backend/demo/src/test/kotlin/com/example/demo/ApplicationControllerTests.kt
@@ -1,14 +1,23 @@
 package com.example.demo
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 
 @SpringBootTest
-class ApplicationControllerTests (
+class ApplicationControllerTests @Autowired constructor(
+    val controller: ApplicationController
     ) {
 
     @Test
     fun contextLoads() {
+    }
+
+    @Test
+    fun connectTestTF() {
+        val returnValue = controller.connectTest()
+        Assertions.assertEquals("test received", returnValue)
     }
 }
