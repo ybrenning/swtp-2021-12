cae7bf0edc1fc8a3d9b1cbce0935a563b085e874
yb63tadu <yb63tadu@studserv.uni-leipzig.de>
2022-03-22 16:39:23 +0100

    Fixed energy and time data in tests

--- backend/demo/src/test/kotlin/com/example/demo/FunctionServiceTests.kt
+++ backend/demo/src/test/kotlin/com/example/demo/FunctionServiceTests.kt
@@ -11,19 +11,17 @@ import org.springframework.boot.test.context.SpringBootTest
 class FunctionServiceTests @Autowired constructor(
     private val service: FunctionValueCalculatorService
 ){
-
     private val functions = arrayListOf("kanzi.Global$1.compare", "kanzi.Global.<clinit>")
     private val configs = arrayListOf("root", "CHECKSUM")
 
-    private val configuredFunction1 = ConfiguredFunction("kanzi.Global$1.compare", 0.17315721808227894, 0.09236394624248921)
-    private val configuredFunction2 = ConfiguredFunction("kanzi.Global.<clinit>", 15.618572522029455, 8.61869990570836)
+    private val configuredFunction1 = ConfiguredFunction("kanzi.Global$1.compare", 0.09236394624248921, 0.17315721808227894)
+    private val configuredFunction2 = ConfiguredFunction("kanzi.Global.<clinit>", 8.61869990570836, 15.618572522029455)
     private val configuredFunctions = arrayListOf(configuredFunction1, configuredFunction2)
 
     @Test
     fun contextLoads() {
     }
 
-    /*
     @Test
     fun calcFunctionValuesTF() {
         val requestReturn: ArrayList<ConfiguredFunction> = service.calcFunctionValues("kanzi", functions, configs)
@@ -39,5 +37,4 @@ class FunctionServiceTests @Autowired constructor(
             Assertions.assertTrue(returnList.contains("kanzi.Global.computeHistogramOrder0"))
         }
     }
-     */
 }
